fc556dd3734b3bb87f66b4ecd8fdc9cebf189dd9
Joseph Wakeling <sc21jjfw@leeds.ac.uk>
2024-03-20 12:56:56 +0000

    Added public site to allowed hosts

--- cwk1/settings.py
+++ cwk1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^b9w&e=60q-k$+(_5(@n-ka_pu_e^%d$ccdr=v*+gae6v!j5lw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["sc21jjfw.pythonanywhere.com"]
 
 
 # Application definition
